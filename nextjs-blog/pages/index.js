6f7e8e9c7b31889c3860549ec5650223c2d214f7
Benjamin Nsingi <b.nsingi07@gmail.com>
2023-07-21 14:31:35 +0200

    fix first commit !

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import ListPage from "./ListPage";
+export default function Home() {
+  return (
+    <div className="container d-flex justify-content-center">
+        <ListPage/>
+    </div>
+  )
+}
